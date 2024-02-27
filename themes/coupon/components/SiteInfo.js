445157312142d92e056993dde836ca2fbb621894
Lyon Liu <ayon.liu@gmail.com>
2024-02-27 18:05:35 +0800

    update coupon theme

--- themes/coupon/components/SiteInfo.js
+++ themes/coupon/components/SiteInfo.js
@@ -8,7 +8,7 @@ function SiteInfo ({ title }) {
 
   return (
         <footer
-            className='relative leading-6 justify-start w-full text-gray-600 dark:text-gray-300 text-xs font-sans'
+            className='relative leading-6 justify-start w-full text-gray-600 dark:text-gray-300 text-xs '
         >
             <span> © {`${copyrightDate}`} <span> <a href={siteConfig('LINK')}> <i className='mx-1 animate-pulse fas fa-heart'/> {siteConfig('AUTHOR')}</a>. <br /></span>
 
@@ -17,8 +17,10 @@ function SiteInfo ({ title }) {
             <span className='hidden busuanzi_container_site_pv'> <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
             <span className='pl-2 hidden busuanzi_container_site_uv'> <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
             <br />
-            {/* <span className='text-xs font-serif'> Powered by <a href='https://github.com/tangly1024/NotionNext' className='underline'>NotionNext {siteConfig('VERSION')}</a></span><br /></span>
-            <h1>{title}</h1> */}
+            {/* 
+            <span className='text-xs font-serif'> Powered by <a href='https://github.com/tangly1024/NotionNext' className='underline'>NotionNext {siteConfig('VERSION')}</a></span><br /></span>
+            <h1>{title}</h1>
+             */}
             </span>
         </footer>
   )
